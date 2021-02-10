d4739c79e8a4f7005f3e91b223c2a81abbaf9eb3
孙轶伟 <sunyiwei.24601@bytedance.com>
2021-02-10 12:51:34 +0800

    init

---
+++ query/showapi.py
@@ -0,0 +1,13 @@
+import requests
+class ShowApiQueryer:
+    def __init__(self, url, appid, secret):
+        self.url = url
+        self.appid = appid
+        self.secret = secret
+
+
+    def queryBook(self, isbn):
+        pass
+
+    def extract(self, bookInfo):
+        pass
